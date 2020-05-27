137cf75853e3c7bfe8a0cb4497cf809762a17288
zyj <1836686674@qq.com>
2020-05-27 08:07:57 +0800

    资讯分类

--- src/main/java/com/niit/soft/client/api/controller/InfoManageController.java
+++ src/main/java/com/niit/soft/client/api/controller/InfoManageController.java
@@ -1,7 +1,9 @@
 package com.niit.soft.client.api.controller;
 
 import com.niit.soft.client.api.domain.model.InfoManage;
+import com.niit.soft.client.api.domain.model.InfoType;
 import com.niit.soft.client.api.service.InfoManageService;
+import com.niit.soft.client.api.service.InfoTypeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +15,11 @@ import java.util.List;
 /**
  * @author Yujie_Zhao
  * @ClassName InfoManageController
- * @Description TODO
+ * @Description 资讯Controller层
  * @Date 2020/5/26  11:52
  * @Version 1.0
  **/
+
 @RestController
 @Slf4j
 @RequestMapping(value = "/info")
@@ -24,13 +27,34 @@ public class InfoManageController {
     @Resource
     private InfoManageService infoManageService;
 
+    @Resource
+    private InfoTypeService infoTypeService;
+
+    /**
+     * 查找所有资讯
+     * @return List<InfoManage>
+     */
     @GetMapping(value = "/all")
-    public List<InfoManage>getAllInfo(){
+    public List<InfoManage> getAllInfo(){
         return infoManageService.getAllInfo();
     }
 
+    /**
+     * 查询置顶资讯
+     * @return  List<InfoManage>
+     */
     @GetMapping(value = "/isTap")
-    public List<InfoManage>getIsTopInfo(){
+    public List<InfoManage> getIsTopInfo(){
         return infoManageService.getIsTopInfo();
+    }
+
+    /**
+     * 资讯分类查询
+     * @param id
+     * @return InfoType
+     */
+    @GetMapping(value = "/type")
+    public InfoType getInfoByType(Long id){
+        return infoTypeService.getInfoByType(id);
     }
 }
---
+++ src/main/java/com/niit/soft/client/api/service/impl/InfoTypeServiceImpl.java
@@ -0,0 +1,51 @@
+package com.niit.soft.client.api.service.impl;
+
+import com.niit.soft.client.api.domain.model.InfoManage;
+import com.niit.soft.client.api.domain.model.InfoMangeType;
+import com.niit.soft.client.api.domain.model.InfoType;
+import com.niit.soft.client.api.repository.InfoManageRepository;
+import com.niit.soft.client.api.repository.InfoMangeTypeRepository;
+import com.niit.soft.client.api.repository.InfoTypeRepository;
+import com.niit.soft.client.api.service.InfoTypeService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author Yujie_Zhao
+ * @ClassName InfoTypeServiceImpl
+ * @Description 资讯分类Service实现类
+ * @Date 2020/5/26  22:05
+ * @Version 1.0
+ **/
+@Service
+public class InfoTypeServiceImpl implements InfoTypeService {
+
+    @Resource
+    private InfoTypeRepository infoTypeRepository;
+    @Resource
+    private InfoMangeTypeRepository infoMangeTypeRepository;
+    @Resource
+    private InfoManageRepository infoManageRepository;
+
+
+    /**
+     * 根据资讯分类id查找对应所有资讯
+     * @param id
+     * @return InfoType
+     */
+    @Override
+    public InfoType getInfoByType(Long id) {
+        InfoType infoType = infoTypeRepository.findByPkInfoTypeId(id);
+        List<InfoMangeType> infoMangeTypes = infoMangeTypeRepository.findAll();
+        //循环所有的资讯分类关联信息，根据分类id找到对应资讯，放入资讯分类中
+        infoMangeTypes.forEach(infoType1 -> {
+            if (infoType1.getTypeId().equals(id)){
+                List<InfoManage> mapList =infoManageRepository.getInfoByType(infoType1.getInfoId());
+                infoType.setInfoManageList(mapList);
+            }
+        });
+        return infoType;
+    }
+}
