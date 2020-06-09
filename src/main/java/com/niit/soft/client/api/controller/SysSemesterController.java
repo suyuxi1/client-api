1d0123f6a2ab3a97a871e1555a08ab9ab5dab5fb
zhent1106 <1299088269@qq.com>
2020-06-09 08:14:13 +0800

    修改

--- src/main/java/com/niit/soft/client/api/controller/SysSemesterController.java
+++ src/main/java/com/niit/soft/client/api/controller/SysSemesterController.java
@@ -4,7 +4,7 @@ import com.niit.soft.client.api.common.ResponseResult;
 import com.niit.soft.client.api.service.SysSemesterService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +25,7 @@ public class SysSemesterController {
     private SysSemesterService sysSemesterService;
 
     @ApiOperation(value = "查询所有学期接口",notes = "")
-    @GetMapping(value = "/all")
+    @PostMapping(value = "/all")
     ResponseResult findAllSysSemester(){
         return sysSemesterService.findAllSysSemester();
     }
