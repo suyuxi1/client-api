0938065c512f830d1f8d7a25f566553fcce67f80
ntt <1914866205@qq.com>
2020-06-09 08:32:02 +0800

    合并

--- src/main/java/com/niit/soft/client/api/domain/model/SysBorrow.java
+++ src/main/java/com/niit/soft/client/api/domain/model/SysBorrow.java
@@ -70,11 +70,6 @@ public class SysBorrow {
     @Column(nullable = false)
     private Timestamp gmtCreate;
 
-    /**
-     * 归还时间
-     */
-    @Column(nullable = false)
-    private Timestamp gmtReturn;
 
     /**
      * 更新时间
