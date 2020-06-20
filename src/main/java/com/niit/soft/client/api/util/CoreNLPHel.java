50bb093fa048e53b5d2e2dcbbb38ba0c3e1d762d
xiaobinggan <1294898797@qq.com>
2020-06-20 21:02:01 +0800

    合并代码

--- src/main/java/com/niit/soft/client/api/util/CoreNLPHel.java
+++ src/main/java/com/niit/soft/client/api/util/CoreNLPHel.java
@@ -1,31 +1,31 @@
-package com.niit.soft.client.api.util;
-
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-
-/**
- * @ClassName CoreNLPHel
- * @Description TODO
- * @Author xiaobinggan
- * @Date 2020/6/20 10:59 上午
- * @Version 1.0
- **/
-
-public class CoreNLPHel {
-    private static CoreNLPHel instance = new CoreNLPHel();
-    private StanfordCoreNLP pipeline;
-
-    private CoreNLPHel() {
-        String props = "CoreNLP-chinese.properties";  //第三步骤的配置文件，放在main/resources目录下
-        pipeline = new StanfordCoreNLP(props);
-    }
-
-    ;
-
-    public static CoreNLPHel getInstance() {
-        return instance;
-    }
-
-    public StanfordCoreNLP getPipeline() {
-        return pipeline;
-    }
-}
+//package com.niit.soft.client.api.util;
+//
+//import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+//
+///**
+// * @ClassName CoreNLPHel
+// * @Description TODO
+// * @Author xiaobinggan
+// * @Date 2020/6/20 10:59 上午
+// * @Version 1.0
+// **/
+//
+//public class CoreNLPHel {
+//    private static CoreNLPHel instance = new CoreNLPHel();
+//    private StanfordCoreNLP pipeline;
+//
+//    private CoreNLPHel() {
+//        String props = "CoreNLP-chinese.properties";  //第三步骤的配置文件，放在main/resources目录下
+//        pipeline = new StanfordCoreNLP(props);
+//    }
+//
+//    ;
+//
+//    public static CoreNLPHel getInstance() {
+//        return instance;
+//    }
+//
+//    public StanfordCoreNLP getPipeline() {
+//        return pipeline;
+//    }
+//}
--- src/main/java/com/niit/soft/client/api/util/Segmentation.java
+++ src/main/java/com/niit/soft/client/api/util/Segmentation.java
@@ -1,48 +1,48 @@
-package com.niit.soft.client.api.util;
-
-/**
- * @ClassName Segmentation
- * @Description TODO
- * @Author xiaobinggan
- * @Date 2020/6/20 10:59 上午
- * @Version 1.0
- **/
-
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.util.CoreMap;
-
-import java.util.List;
-
-public class Segmentation {
-    private String segtext = "";
-
-    public Segmentation(String text) {
-        CoreNLPHel coreNLPHel = CoreNLPHel.getInstance();
-        StanfordCoreNLP pipeline = coreNLPHel.getPipeline();
-        Annotation annotation = new Annotation(text);
-        pipeline.annotate(annotation);
-        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-        //ArrayList<String> array = new ArrayList<String>();
-        StringBuffer sb = new StringBuffer();
-        for (CoreMap sentence : sentences) {
-            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                sb.append(word);
-                sb.append(" ");
-            }
-        }
-        segtext = sb.toString().trim();
-        //segtext = array.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Segmentation("在寂寞的时候又会想起他").getSegtext());
-    }
-
-    public String getSegtext() {
-        return segtext;
-    }
-}
+//package com.niit.soft.client.api.util;
+//
+///**
+// * @ClassName Segmentation
+// * @Description TODO
+// * @Author xiaobinggan
+// * @Date 2020/6/20 10:59 上午
+// * @Version 1.0
+// **/
+//
+//import edu.stanford.nlp.ling.CoreAnnotations;
+//import edu.stanford.nlp.ling.CoreLabel;
+//import edu.stanford.nlp.pipeline.Annotation;
+//import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+//import edu.stanford.nlp.util.CoreMap;
+//
+//import java.util.List;
+//
+//public class Segmentation {
+//    private String segtext = "";
+//
+//    public Segmentation(String text) {
+//        CoreNLPHel coreNLPHel = CoreNLPHel.getInstance();
+//        StanfordCoreNLP pipeline = coreNLPHel.getPipeline();
+//        Annotation annotation = new Annotation(text);
+//        pipeline.annotate(annotation);
+//        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+//        //ArrayList<String> array = new ArrayList<String>();
+//        StringBuffer sb = new StringBuffer();
+//        for (CoreMap sentence : sentences) {
+//            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+//                String word = token.get(CoreAnnotations.TextAnnotation.class);
+//                sb.append(word);
+//                sb.append(" ");
+//            }
+//        }
+//        segtext = sb.toString().trim();
+//        //segtext = array.toString();
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(new Segmentation("在寂寞的时候又会想起他").getSegtext());
+//    }
+//
+//    public String getSegtext() {
+//        return segtext;
+//    }
+//}
